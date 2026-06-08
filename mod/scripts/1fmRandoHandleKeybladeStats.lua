190f8555e818df05a8998a4f9274dae4975ed48a
gaithern <gaithern@gmail.com>
2026-06-08 16:01:36 -0500

    Update this to not bother with item descriptions, it really needs to be handled as a file.

--- mod/scripts/1fmRandoHandleKeybladeStats.lua
+++ mod/scripts/1fmRandoHandleKeybladeStats.lua
@@ -5,16 +5,9 @@ LUAGUI_DESC = "Kingdom Hearts 1FM Handle Keyblade Stats"
 local kh1_lua_library = require("kh1_lua_library")
 local seed_vars = require("seed_vars")
 
-local function handle_item_descriptions()
-    kh1_lua_library.set_absolute_item_description_at_idx_memory_address(0, "Let's try something SUPER long.")
-end
-
 function _OnInit()
     if GAME_ID == 0xAF71841E and ENGINE_TYPE == "BACKEND" then
         require("VersionCheck")
-        if canExecute then
-            handle_item_descriptions()
-        end
     else
         ConsolePrint("KH1 not detected, not running script")
     end
