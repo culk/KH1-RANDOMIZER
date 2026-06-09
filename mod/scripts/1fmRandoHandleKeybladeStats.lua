0c1f6395cfbc18d5928d412a5bd8cb88c06ae469
gaithern <gaithern@gmail.com>
2026-06-08 19:19:14 -0500

    Add handle keyblade stats logic

--- mod/scripts/1fmRandoHandleKeybladeStats.lua
+++ mod/scripts/1fmRandoHandleKeybladeStats.lua
@@ -5,9 +5,28 @@ LUAGUI_DESC = "Kingdom Hearts 1FM Handle Keyblade Stats"
 local kh1_lua_library = require("kh1_lua_library")
 local seed_vars = require("seed_vars")
 
+local init_offsets = {
+    ["STR"] = 0x9528,
+    ["CRR"] = 0x9529,
+    ["CRB"] = 0x952A,
+    ["REC"] = 0x952B,
+    ["MP"]  = 0x9530
+}
+
+local function write_keyblade_stats()
+    for keyblade, stats in pairs(seed_vars["keyblade_stats"]) do
+        for stat, value in pairs(stats) do
+            WriteByte(jumpHeights - 0xAC + init_offsets[stat] + 0x58 * keyblade, value)
+        end
+    end
+end
+
 function _OnInit()
     if GAME_ID == 0xAF71841E and ENGINE_TYPE == "BACKEND" then
         require("VersionCheck")
+        if canExecute then
+            write_keyblade_stats()
+        end
     else
         ConsolePrint("KH1 not detected, not running script")
     end
